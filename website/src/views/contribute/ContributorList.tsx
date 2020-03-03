ea6f5ce156f1df838d9bee6676cfb3b97297f503
Donald <dlqs@gmx.com>
2020-03-03 13:44:04 +0800

    fix contributors api error message

--- website/src/views/contribute/ContributorList.tsx
+++ website/src/views/contribute/ContributorList.tsx
@@ -51,7 +51,7 @@ const ContributorList = Loadable.Map<Props, Export>({
   },
   loading: (props: LoadingComponentProps) => {
     if (props.error) {
-      return <ApiError dataName="venue locations" retry={props.retry} />;
+      return <ApiError dataName="contributors list" retry={props.retry} />;
     }
     if (props.pastDelay) {
       return <LoadingSpinner />;
